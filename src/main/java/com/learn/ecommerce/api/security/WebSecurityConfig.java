4c8d28ede4e35e28caf7f1f3960c78bf235d61a7
Mohamed Salah <mohamedelmorgel2001@gmail.com>
2025-12-07 13:45:25 +0200

    Enhance security configuration to include JWT authentication filter and restrict access to login endpoint

--- src/main/java/com/learn/ecommerce/api/security/WebSecurityConfig.java
+++ src/main/java/com/learn/ecommerce/api/security/WebSecurityConfig.java
@@ -3,21 +3,38 @@ package com.learn.ecommerce.api.security;
 import org.apache.catalina.filters.HttpHeaderSecurityFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class WebSecurityConfig {
+
+    private JwtRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)  throws Exception {
             http
                 .csrf(csrf->csrf.disable())
-                .cors(cors->cors.disable());
-            http
-                .authorizeHttpRequests(request-> request.anyRequest().permitAll());
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authRequest->
+                        authRequest.requestMatchers("/auth/login")
+                                .permitAll()
+                                .anyRequest().authenticated())
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+
 
         return http.build();
 
     }
+
 }
