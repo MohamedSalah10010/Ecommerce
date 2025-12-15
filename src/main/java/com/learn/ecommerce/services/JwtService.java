128ac8771509a971dc4d21179fa27e0bff93c3d0
Mohamed Salah <mohamedelmorgel2001@gmail.com>
2025-12-15 23:46:50 +0200

    Refactor JwtService to use DecodedJWT for token verification in getUsername method

--- src/main/java/com/learn/ecommerce/services/JwtService.java
+++ src/main/java/com/learn/ecommerce/services/JwtService.java
@@ -2,6 +2,7 @@ package com.learn.ecommerce.services;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.learn.ecommerce.model.LocalUser;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -45,6 +46,8 @@ public class JwtService {
     }
 
     public String getUsername(String token) {
-        return JWT.decode(token).getClaim(USERNAME_KEY).asString();
+
+        DecodedJWT jwtDecoded = JWT.require(algorithm).withIssuer(issuer).build().verify(token);
+        return jwtDecoded.getClaim(USERNAME_KEY).asString();
     }
 }
